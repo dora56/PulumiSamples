9f5f2c40de3ddfdd879071e67c437430e96f48bc
Ryu Hanaoka <doradora56@gmail.com>
2023-05-13 07:23:30 +0900

    test: WepAppのテスト追加

--- WebappAndMsSql/tests/IaC.Test/MyStackTest.cs
+++ WebappAndMsSql/tests/IaC.Test/MyStackTest.cs
@@ -26,51 +26,100 @@ public class MyStackTest
     [Test]
     public async Task SingleResourceGroupExists()
     {
+        // Arrange
         var resources = await TestAsync();
-
+        
+        // Act
         var resourceGroups = resources.OfType<ResourceGroup>().ToList();
+        
+        // Assert
         resourceGroups.Count.Should().Be(1, "a single resource group is expected");
     }
     
     [Test]
     public async Task SingleSqlServerExists()
     {
+        // Arrange
         var resources = await TestAsync();
-
+        
+        // Act
         var sqlServers = resources.OfType<Server>().ToList();
+        
+        // Assert
         sqlServers.Count.Should().Be(1, "a single sql server is expected");
     }
     
     [Test]
     public async Task SingleSqlServerName()
     {
+        // Arrange
         var config = new Dictionary<string, object>();
         config.Add("azure-native:resourceName","testWeb");
         var resources = await TestAsync(config);
 
+        // Act
         var sqlServers = resources.OfType<Server>().ToList();
         var name = await sqlServers.FirstOrDefault()!.Name.GetValueAsync();
+        
+        // Assert
         name.Should().Be("sql-testWeb");
     }
     // Create SqlDatabase Test
     [Test]
     public async Task SingleSqlDatabaseExists()
     {
+        // Arrange
         var resources = await TestAsync();
-
+        
+        // Act
         var sqlDatabases = resources.OfType<Database>().ToList();
+        
+        // Assert
         sqlDatabases.Count.Should().Be(1, "a single sql database is expected");
     }
     
     [Test]
     public async Task SingleSqlDatabaseName()
     {
+        // Arrange
+        var config = new Dictionary<string, object>();
+        config.Add("azure-native:resourceName","testWeb");
+        var resources = await TestAsync(config);
+        
+        // Act
+        var sqlDatabases = resources.OfType<Database>().ToList();
+        var name = await sqlDatabases.FirstOrDefault()!.Name.GetValueAsync();
+        
+        // Assert
+        name.Should().Be("sqldb-testWeb");
+    }
+    
+    [Test]
+    public async Task SingleWebAppExists()
+    {
+        // Arrange
+        var resources = await TestAsync();
+        
+        // Act
+        var webApps = resources.OfType<WebApp>().ToList();
+        
+        // Assert
+        webApps.Count.Should().Be(1, "a single web app is expected");
+    }
+    
+    [Test]
+    public async Task SingleWebAppName()
+    {
+        // Arrange
         var config = new Dictionary<string, object>();
         config.Add("azure-native:resourceName","testWeb");
         var resources = await TestAsync(config);
 
-        var sqlDatabases = resources.OfType<Database>().ToList();
-        var name = await sqlDatabases.FirstOrDefault()!.Name.GetValueAsync();
-        name.Should().Be("sqldb-testWeb");
+        // Act
+        var webApps = resources.OfType<WebApp>().ToList();
+        var name = await webApps.FirstOrDefault()!.Name.GetValueAsync();
+        
+        // Assert
+        name.Should().Be("webapp-testWeb");
     }
 }
